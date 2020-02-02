e4eca520e44451f705a7836cdb5fe62d1b77c4a2
TANA <iamtana90@gmail.com>
2020-02-02 14:12:12 +0200

    Initial Commit

---
+++ AWLoadingPopup/AWLoadingPopup.h
@@ -0,0 +1,19 @@
+//
+//  AWLoadingPopup.h
+//  AWLoadingPopup
+//
+//  Created by Tudor Ana on 02/02/2020.
+//  Copyright © 2020 Tudor Ana. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AWLoadingPopup.
+FOUNDATION_EXPORT double AWLoadingPopupVersionNumber;
+
+//! Project version string for AWLoadingPopup.
+FOUNDATION_EXPORT const unsigned char AWLoadingPopupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AWLoadingPopup/PublicHeader.h>
+
+
